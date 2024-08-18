3c994b1d71c081f1ea18c1b76bf50ec3493e0297
Femila Abdul Kareem <sulthan55@Femilas-MacBook-Air.local>
2024-08-18 09:20:49 +0400

    secodn

--- Commands.cpp
+++ Commands.cpp
@@ -6,7 +6,36 @@
 /*   By: fabdul-k <fabdul-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 18:58:33 by fabdul-k          #+#    #+#             */
-/*   Updated: 2024/08/15 19:07:30 by fabdul-k         ###   ########.fr       */
+/*   Updated: 2024/08/17 07:41:11 by fabdul-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Commands.hpp"
+
+Commands::Commands(std::string message, int fd, std::string nickname, std::string username):_message(message), _fd(fd), _nickname(nickname), _username(username)
+{
+    if (!message.empty())
+    {
+        std::istringstream input(message);
+        std::string inputsplit;
+
+        while (std::getline(input, inputsplit, ' ') && inputsplit != " ")
+            args.push_back(inputsplit);
+        _command = args[0];
+        args.erase(args.begin());
+        _command.erase(std::remove(_command.begin(), _command.end(), '\r'), _command.end());
+        _command.erase(std::remove(_command.begin(), _command.end(), '\n'), _command.end());
+        for(std::vector<std::string>::iterator it = args.begin(); it != args.end(); it++)
+        {
+            args.erase(std::remove(args.begin(),args.end(), '\r'),args.end());
+            args.erase(std::remove(args.begin(),args.end(), '\n'),args.end());
+        }
+    }
+}
+
+Commands::~Commands(){}
+
+int Commands::ExecuteCommand()
+{
+    
+}
--- Commands.hpp
+++ Commands.hpp
@@ -6,7 +6,37 @@
 /*   By: fabdul-k <fabdul-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 18:57:49 by fabdul-k          #+#    #+#             */
-/*   Updated: 2024/08/15 19:07:33 by fabdul-k         ###   ########.fr       */
+/*   Updated: 2024/08/17 07:35:23 by fabdul-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+#include <iostream>
+#include <sstream>
+
+class User;
+class Server;
+class Channel;
+class Commands
+{
+    private:
+    int _fd;
+    std::string _message;
+    std::string _username;
+    std::string _nickname;
+    std::string _command;
+
+    std::vector<std::string> args;
+    Commands();
+
+    public:
+    Commands(std::string message, int fd, std::string nickname, std::string username);
+    ~Commands();
+
+    
+
+
+};
+
+#endif
